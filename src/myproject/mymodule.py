fe8eb8742ac26f0dd48dd063aeefc4fbef570f2d
Neal Morton <mortonne@gmail.com>
2023-06-11 10:53:59 -0500

    refactor: move message printing to module function

--- src/myproject/mymodule.py
+++ src/myproject/mymodule.py
@@ -3,6 +3,11 @@
 import click
 
 
+def print_message(message="Hello, world"):
+    """Print a message given as a string."""
+    print(message)
+
+
 @click.command()
 def hello_world():
-    print("Hello, world.")
+    print_message()
